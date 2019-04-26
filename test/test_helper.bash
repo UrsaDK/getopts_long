621640a3e70713fca546f969c2fa8a206e52ffcf
UmkaDK <umka.dk@icloud.com>
2019-04-26 15:45:55 +0200

    Fixed wording when showing DEBUG output

--- test/test_helper.bash
+++ test/test_helper.bash
@@ -3,8 +3,8 @@ PATH="${TOPDIR}/bin:${PATH}"
 FEATURE="$(basename "${BATS_TEST_FILENAME}" '.bats' | tr '_' ' ')"
 
 debug() {
-    printf '\nEXPECTED:\n––––––––\n%s\n' "${1}" >&3
-    printf '\nACTUAL:\n––––––––\n%s\n\n' "${2}" >&3
+    printf '\nACTUAL:\n––––––––\n%s\n' "${2}" >&3
+    printf '\nEXPECTED:\n––––––––\n%s\n\n' "${1}" >&3
 }
 
 expect() {
